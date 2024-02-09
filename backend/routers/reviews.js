f05be9b2bec788a4436b82ba08886d2840791564
purushothaman D <148258087+purushothamanlabi@users.noreply.github.com>
2024-02-09 08:18:30 -0800

    Delete backend directory

--- backend/routers/reviews.js
+++
@@ -1,7 +0,0 @@
-import Express from "express";
-import  {createReview} from "../controler/reviewControler.js";
-const router = Express.Router();
-
-router.post("/:tourId",createReview);  //verifyUser
-
-export default router;
