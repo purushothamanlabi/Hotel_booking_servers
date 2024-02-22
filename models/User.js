df4b9972496c68acdf247d6f7362f0226069255b
purushothaman D <148258087+purushothamanlabi@users.noreply.github.com>
2024-02-22 17:50:23 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      maxlength: 8, 
+      maxlength: 15, 
     },
     email: {
       type: String,
